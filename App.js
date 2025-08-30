eec4a458bfa582b4cd2aa05ae13cc11e1c05e858
Abdul Manan <abdullmanann7777@gmail.com>
2025-08-30 16:26:19 +0500

    Update App.js

--- App.js
+++ App.js
@@ -1,11 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const[bgcolor,setbgcolor]=useState("white");
+ 
+    const generateRandomColor = () => {
+  const r = Math.floor(Math.random() * 256);
+  const g = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+  return `rgb(${r},${g},${b})`;
+}
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={[styles.container,{backgroundColor:bgcolor}]}>
+      <Button style={styles.buttonText} onPress={()=>setbgcolor(generateRandomColor)}title='Change Color' />    
+        <StatusBar style="auto" />
     </View>
   );
 }
@@ -13,8 +22,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+   
+  buttonText: {
+    fontSize: 18,
+    fontWeight: "bold",
   },
 });
